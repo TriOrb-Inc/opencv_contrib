d60326c715c8fa51ce99b791f8c4bae848fa4c91
Tobeta <tobeta.masakazu@triorb.co.jp>
2023-09-29 06:37:00 +0000

    CV_EXPORTS -> CV_EXPORTS_W

--- modules/sfm/include/opencv2/sfm/reconstruct.hpp
+++ modules/sfm/include/opencv2/sfm/reconstruct.hpp
@@ -49,93 +49,93 @@
 
 namespace cv
 {
-namespace sfm
-{
+  namespace sfm
+  {
 
-//! @addtogroup reconstruction
-//! @{
+    //! @addtogroup reconstruction
+    //! @{
 
 #if defined(CV_DOXYGEN) || defined(CERES_FOUND)
 
-/** @brief Reconstruct 3d points from 2d correspondences while performing autocalibration.
-  @param points2d Input vector of vectors of 2d points (the inner vector is per image).
-  @param Ps Output vector with the 3x4 projections matrices of each image.
-  @param points3d Output array with estimated 3d points.
-  @param K Input/Output camera matrix \f$K = \vecthreethree{f_x}{0}{c_x}{0}{f_y}{c_y}{0}{0}{1}\f$. Input parameters used as initial guess.
-  @param is_projective if true, the cameras are supposed to be projective.
+    /** @brief Reconstruct 3d points from 2d correspondences while performing autocalibration.
+      @param points2d Input vector of vectors of 2d points (the inner vector is per image).
+      @param Ps Output vector with the 3x4 projections matrices of each image.
+      @param points3d Output array with estimated 3d points.
+      @param K Input/Output camera matrix \f$K = \vecthreethree{f_x}{0}{c_x}{0}{f_y}{c_y}{0}{0}{1}\f$. Input parameters used as initial guess.
+      @param is_projective if true, the cameras are supposed to be projective.
 
-  This method calls below signature and extracts projection matrices from estimated K, R and t.
+      This method calls below signature and extracts projection matrices from estimated K, R and t.
 
-   @note
-    - Tracks must be as precise as possible. It does not handle outliers and is very sensible to them.
-*/
-CV_EXPORTS
-void
-reconstruct(InputArrayOfArrays points2d, OutputArray Ps, OutputArray points3d, InputOutputArray K,
-            bool is_projective = false);
+       @note
+        - Tracks must be as precise as possible. It does not handle outliers and is very sensible to them.
+    */
+    CV_EXPORTS_W
+    void
+    reconstruct(InputArrayOfArrays points2d, OutputArray Ps, OutputArray points3d, InputOutputArray K,
+                bool is_projective = false);
 
-/** @brief Reconstruct 3d points from 2d correspondences while performing autocalibration.
-  @param points2d Input vector of vectors of 2d points (the inner vector is per image).
-  @param Rs Output vector of 3x3 rotations of the camera.
-  @param Ts Output vector of 3x1 translations of the camera.
-  @param points3d Output array with estimated 3d points.
-  @param K Input/Output camera matrix \f$K = \vecthreethree{f_x}{0}{c_x}{0}{f_y}{c_y}{0}{0}{1}\f$. Input parameters used as initial guess.
-  @param is_projective if true, the cameras are supposed to be projective.
+    /** @brief Reconstruct 3d points from 2d correspondences while performing autocalibration.
+      @param points2d Input vector of vectors of 2d points (the inner vector is per image).
+      @param Rs Output vector of 3x3 rotations of the camera.
+      @param Ts Output vector of 3x1 translations of the camera.
+      @param points3d Output array with estimated 3d points.
+      @param K Input/Output camera matrix \f$K = \vecthreethree{f_x}{0}{c_x}{0}{f_y}{c_y}{0}{0}{1}\f$. Input parameters used as initial guess.
+      @param is_projective if true, the cameras are supposed to be projective.
 
-  Internally calls libmv simple pipeline routine with some default parameters by instatiating SFMLibmvEuclideanReconstruction class.
+      Internally calls libmv simple pipeline routine with some default parameters by instatiating SFMLibmvEuclideanReconstruction class.
 
-  @note
-    - Tracks must be as precise as possible. It does not handle outliers and is very sensible to them.
-    - To see a working example for camera motion reconstruction, check the following tutorial: @ref tutorial_sfm_trajectory_estimation.
-*/
-CV_EXPORTS
-void
-reconstruct(InputArrayOfArrays points2d, OutputArray Rs, OutputArray Ts, InputOutputArray K,
-            OutputArray points3d, bool is_projective = false);
+      @note
+        - Tracks must be as precise as possible. It does not handle outliers and is very sensible to them.
+        - To see a working example for camera motion reconstruction, check the following tutorial: @ref tutorial_sfm_trajectory_estimation.
+    */
+    CV_EXPORTS_W
+    void
+    reconstruct(InputArrayOfArrays points2d, OutputArray Rs, OutputArray Ts, InputOutputArray K,
+                OutputArray points3d, bool is_projective = false);
 
-/** @brief Reconstruct 3d points from 2d images while performing autocalibration.
-  @param images a vector of string with the images paths.
-  @param Ps Output vector with the 3x4 projections matrices of each image.
-  @param points3d Output array with estimated 3d points.
-  @param K Input/Output camera matrix \f$K = \vecthreethree{f_x}{0}{c_x}{0}{f_y}{c_y}{0}{0}{1}\f$. Input parameters used as initial guess.
-  @param is_projective if true, the cameras are supposed to be projective.
+    /** @brief Reconstruct 3d points from 2d images while performing autocalibration.
+      @param images a vector of string with the images paths.
+      @param Ps Output vector with the 3x4 projections matrices of each image.
+      @param points3d Output array with estimated 3d points.
+      @param K Input/Output camera matrix \f$K = \vecthreethree{f_x}{0}{c_x}{0}{f_y}{c_y}{0}{0}{1}\f$. Input parameters used as initial guess.
+      @param is_projective if true, the cameras are supposed to be projective.
 
-  This method calls below signature and extracts projection matrices from estimated K, R and t.
+      This method calls below signature and extracts projection matrices from estimated K, R and t.
 
-   @note
-    - The images must be ordered as they were an image sequence. Additionally, each frame should be as close as posible to the previous and posterior.
-    - For now DAISY features are used in order to compute the 2d points tracks and it only works for 3-4 images.
-*/
-CV_EXPORTS
-void
-reconstruct(const std::vector<String> images, OutputArray Ps, OutputArray points3d,
-            InputOutputArray K, bool is_projective = false);
+       @note
+        - The images must be ordered as they were an image sequence. Additionally, each frame should be as close as posible to the previous and posterior.
+        - For now DAISY features are used in order to compute the 2d points tracks and it only works for 3-4 images.
+    */
+    CV_EXPORTS_W
+    void
+    reconstruct(const std::vector<String> images, OutputArray Ps, OutputArray points3d,
+                InputOutputArray K, bool is_projective = false);
 
-/** @brief Reconstruct 3d points from 2d images while performing autocalibration.
-  @param images a vector of string with the images paths.
-  @param Rs Output vector of 3x3 rotations of the camera.
-  @param Ts Output vector of 3x1 translations of the camera.
-  @param points3d Output array with estimated 3d points.
-  @param K Input/Output camera matrix \f$K = \vecthreethree{f_x}{0}{c_x}{0}{f_y}{c_y}{0}{0}{1}\f$. Input parameters used as initial guess.
-  @param is_projective if true, the cameras are supposed to be projective.
+    /** @brief Reconstruct 3d points from 2d images while performing autocalibration.
+      @param images a vector of string with the images paths.
+      @param Rs Output vector of 3x3 rotations of the camera.
+      @param Ts Output vector of 3x1 translations of the camera.
+      @param points3d Output array with estimated 3d points.
+      @param K Input/Output camera matrix \f$K = \vecthreethree{f_x}{0}{c_x}{0}{f_y}{c_y}{0}{0}{1}\f$. Input parameters used as initial guess.
+      @param is_projective if true, the cameras are supposed to be projective.
 
-  Internally calls libmv simple pipeline routine with some default parameters by instatiating SFMLibmvEuclideanReconstruction class.
+      Internally calls libmv simple pipeline routine with some default parameters by instatiating SFMLibmvEuclideanReconstruction class.
 
-   @note
-    - The images must be ordered as they were an image sequence. Additionally, each frame should be as close as posible to the previous and posterior.
-    - For now DAISY features are used in order to compute the 2d points tracks and it only works for 3-4 images.
-    - To see a working example for scene reconstruction, check the following tutorial: @ref tutorial_sfm_scene_reconstruction.
-*/
-CV_EXPORTS
-void
-reconstruct(const std::vector<String> images, OutputArray Rs, OutputArray Ts,
-            InputOutputArray K, OutputArray points3d, bool is_projective = false);
+       @note
+        - The images must be ordered as they were an image sequence. Additionally, each frame should be as close as posible to the previous and posterior.
+        - For now DAISY features are used in order to compute the 2d points tracks and it only works for 3-4 images.
+        - To see a working example for scene reconstruction, check the following tutorial: @ref tutorial_sfm_scene_reconstruction.
+    */
+    CV_EXPORTS_W
+    void
+    reconstruct(const std::vector<String> images, OutputArray Rs, OutputArray Ts,
+                InputOutputArray K, OutputArray points3d, bool is_projective = false);
 
 #endif /* CV_DOXYGEN || CERES_FOUND */
 
-//! @} sfm
+    //! @} sfm
 
-} /* namespace cv */
+  } /* namespace cv */
 } /* namespace sfm */
 
 #endif
